1f9fc891f114485ab383849892ebcaaafeb040a6
Lucas Souza <lucassouza690@gmail.com>
2020-02-25 18:34:49 -0300

    Metodos atualiza quantidade no DB

--- Aulas/Aula1/antes/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula1/antes/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -8,13 +8,23 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            var itemPedidoDb = dbSet.Where(x => x.Id == itemPedido.Id).SingleOrDefault();
+            if(itemPedidoDb != null)
+            {
+                itemPedidoDb.AtualizaQuantidade(itemPedido.Quantidade);
+                contexto.SaveChanges();
+            }
         }
     }
 }
